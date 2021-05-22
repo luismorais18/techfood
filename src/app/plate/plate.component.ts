45d83d882787e9fe763dbd5e3a1f8028c44d29c3
Luis Morais <luismorais18@protonmail.com>
2021-05-22 14:58:55 +0100

    Logica de aumentar/reduzir unidades e botao de retroceder a funcionar

--- src/app/plate/plate.component.ts
+++ src/app/plate/plate.component.ts
@@ -7,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlateComponent implements OnInit {
 
+  plateQuantity: number = 1;
+  platePrice: number = 12.34; // TODO This variable will be filled from info of a database or any other data repository
+  platePriceDisplay: number = this.platePrice; // Variable that will show the price to the customer
+
   constructor() { }
 
   ngOnInit() {}
+
+  incrementQuantity() {
+    this.plateQuantity++;
+    this.updatePrice()
+
+  }
+
+  decrementQuantity() {
+    this.plateQuantity--;
+    this.updatePrice();
+  }
+
+  updatePrice() {
+    this.platePriceDisplay = Math.round((this.platePrice * this.plateQuantity) * 100) / 100; // Round to two decimal places
+  }
 
 }
