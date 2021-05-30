ea2cc5b0e1a8b1b5c34931b7008dca36a0350991
Luis Morais <luismorais18@protonmail.com>
2021-05-30 16:00:25 +0100

    Fim da tarefa media

--- src/app/cart/cart-plate/cart-plate.component.ts
+++ src/app/cart/cart-plate/cart-plate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { PlateService } from 'src/app/services/cart/plate.service';
 
 @Component({
@@ -9,15 +10,68 @@ import { PlateService } from 'src/app/services/cart/plate.service';
 export class CartPlateComponent implements OnInit {
 
   ingredientList: any[];
+  ingredientListOriginal: any[];
+  changedPlate: boolean = false;
 
-  constructor(private plateService: PlateService) { }
+
+  constructor(private plateService: PlateService, public toastController: ToastController) { }
 
   ngOnInit() {
 
     this.plateService.getIngredients().subscribe((response) => {
       this.ingredientList = response;
-
-    })
+    });
   }
 
+  increaseQuantity(id: number) {
+    for (let i = 0; i < this.ingredientList.length; i++) {
+      if(this.ingredientList[i].id == id) {
+        this.ingredientList[i].quantity++;
+      }
+    }
+
+    if(this.ingredientList === this.ingredientListOriginal) {
+      this.changedPlate = false;
+    } else {
+      this.changedPlate = true;
+    }
+  }
+
+  decreaseQuantity(id: number) {
+    for (let i = 0; i < this.ingredientList.length; i++) {
+      if(this.ingredientList[i].id == id && this.ingredientList[i].quantity > 0) {
+        this.ingredientList[i].quantity--;
+      }
+    }
+    if(this.ingredientList === this.ingredientListOriginal) {
+      this.changedPlate = false;
+    } else {
+      this.changedPlate = true;
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Não é possível guardar um prato sem ingredientes',
+      duration: 1500
+    });
+    toast.present();
+  }
+
+
+  save() {
+
+    let ingredientQuantity = 0;
+    for (let i = 0; i < this.ingredientList.length; i++) {
+      if(this.ingredientList[i].quantity == 0) {
+        ingredientQuantity++;
+      }
+    }
+    if(ingredientQuantity == this.ingredientList.length) {
+      this.presentToast();
+    } else {
+
+
+    }
+  }
 }
